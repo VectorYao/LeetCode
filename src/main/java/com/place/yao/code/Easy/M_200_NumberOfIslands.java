24efad4542aef530c4fe6e675b38030707b3c816
yaoweichang <yaoweichang@youzan.com>
2022-02-23 17:04:08 +0800

    commit m200

---
+++ src/main/java/com/place/yao/code/Easy/M_200_NumberOfIslands.java
@@ -0,0 +1,43 @@
+package com.place.yao.code.Easy;
+
+/**
+ * @Description 岛屿的数量 (https://leetcode.com/problems/number-of-islands/)
+ * @Author vectoryao
+ * @Date 2022/2/23
+ **/
+public class M_200_NumberOfIslands {
+
+    public static void main(String[] args) {
+        char[][] grid = new char[][]{
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+        System.out.println(numIslands(grid));
+    }
+
+    private static int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+}
